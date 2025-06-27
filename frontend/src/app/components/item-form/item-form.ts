4aa20d1b2780ea2791b8e15849467446494c1c69
Timotej <timotej.gerzelj@gmail.com>
2025-06-27 13:45:33 +0200

    Add #2: Added form fields

---
+++ frontend/src/app/components/item-form/item-form.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-item-form',
+  imports: [],
+  templateUrl: './item-form.html',
+  styleUrl: './item-form.css'
+})
+export class ItemForm {
+
+}
--- frontend/src/app/views/create-item/create-item.ts
+++ frontend/src/app/views/create-item/create-item.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ItemForm } from '../../components/item-form/item-form';
 
 @Component({
+  standalone: true,
   selector: 'app-create-item',
-  imports: [],
+  imports: [ItemForm],
   templateUrl: './create-item.html',
   styleUrl: './create-item.css'
 })
