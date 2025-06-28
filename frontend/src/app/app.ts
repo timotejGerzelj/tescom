cb563db231e951809dd0fb6081e3424ee643a4c6
Timotej <timotej.gerzelj@gmail.com>
2025-06-28 14:30:38 +0200

    Add #1: DaisyUI dependency added.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CreateItem } from "./views/create-item/create-item";
 
 @Component({
   selector: 'app-root',
