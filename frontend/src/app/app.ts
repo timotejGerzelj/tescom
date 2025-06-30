49c37d309031a46ccc6473896d63a1c8325dd472
Timotej <timotej.gerzelj@gmail.com>
2025-06-30 15:08:53 +0200

    Start #5: Added navbar component, added styling for navbar.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CreateItem } from "./views/create-item/create-item";
+import { Navbar } from "./views/navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html'
 })
 export class App {
---
+++ frontend/src/app/views/navbar/navbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar {
+
+}
