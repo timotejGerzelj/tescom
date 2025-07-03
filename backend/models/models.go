5bb60122e4dd3f4dc6aaffc0506fbe18c8c53eaf
TImotej <timotej.gerzelj@gmail.com>
2025-07-03 11:51:30 +0200

    Add #7: Add folders controllers, models, routes, basic models added.

---
+++ backend/models/models.go
@@ -0,0 +1,43 @@
+package models
+
+import (
+	"time"
+)
+
+type Item struct {
+	ID          string    `json:"id"`
+	SellerId    string    `json:"sellerId"`
+	Name        string    `json:"name"`
+	Quantity    float64   `json:"quantity`
+	Price       float64   `json:"price"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
+
+type User struct {
+	ID        string    `json:"id"`
+	Role      bool      `json:"role"`
+	Email     string    `json:"email`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type Chat struct {
+	ID        string    `json:"id"`
+	ItemId    string    `json:"role"`
+	UserId    string    `json:"userID`
+	Status    bool      `json:"status"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type ChatMessage struct {
+	ID         string    `json:"id"`
+	ItemId     string    `json:"itemId"`
+	UserId     string    `json:"userId`
+	Message    string    `json:"message`
+	PriceOffer float64   `json:"priceOffer`
+	CreatedAt  time.Time `json:"createdAt"`
+	UpdatedAt  time.Time `json:"updatedAt"`
+}
